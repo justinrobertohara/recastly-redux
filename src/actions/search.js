a7fd5fe49e99de8366ae7561680f1db6ea682e26
Justin O'Hara <justinrobertohara@gmail.com>
2019-06-04 20:52:14 -0700

    started sprint

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,10 +3,15 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
-var handleVideoSearch = (q) => {
- 
+var handleVideoSearch = q => {
   //TODO:  Write an asynchronous action to handle a video search!
+  var options = { YOUTUBE_API_KEY, q };
+
+  var videos = searchYouTube(options);
+
+  return { type: searchYouTube, videos: videos };
+
+  return searchYouTube(options);
 };
 
 export default handleVideoSearch;
