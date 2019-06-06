43cffd406f61b68930444de2c04b531811c9b164
Justin O'Hara <justinrobertohara@gmail.com>
2019-06-05 17:47:39 -0700

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,14 +4,14 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var handleVideoSearch = q => {
-  //TODO:  Write an asynchronous action to handle a video search!
-  var options = { YOUTUBE_API_KEY, q };
+  // //TODO:  Write an asynchronous action to handle a video search!
 
-  var videos = searchYouTube(options);
-
-  return { type: searchYouTube, videos: videos };
-
-  return searchYouTube(options);
+  return dispatch => {
+    dispatch({
+      type: 'HANDLE_VIDEO_SEARCH',
+      videos: searchYouTube({ YOUTUBE_API_KEY, q })
+    });
+  };
 };
 
 export default handleVideoSearch;
